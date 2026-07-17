91d3f49e254caae2d74565ed7cee331a7b286a0e
Reinaldo Junior Rocha <38896311+reinaldorocha@users.noreply.github.com>
2026-07-17 12:19:22 -0300

    Feat/issue 30 visual email editor (#1)
    * feat: add visual email template editor
    
    * feat: improve recovery email defaults
    
    ---------
    
    Co-authored-by: Reinaldo Rocha <reinaldo.sousa@korp.com.br>

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -410,6 +410,7 @@ class ProdutosController extends Controller
 
         return Inertia::render('Produtos/Edit', [
             'produto' => $produtoArray,
+            'default_cart_recovery_email' => Product::defaultCheckoutConfig()['cart_recovery_email'],
             'productTypes' => $productTypes,
             'billingTypes' => $billingTypes,
             'exchange_rates' => $this->legacyExchangeRatesMap($tenantCurrencies),
